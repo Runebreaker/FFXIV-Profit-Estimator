f500cd6d799501c4973621324f88ddd70183de09
Runebreaker <gino.schulze-skyfire@web.de>
2023-03-24 22:23:20 +0400

    API progress

--- shared/src/commonMain/kotlin/com/example/ffxivprofitestimator/App.kt
+++ shared/src/commonMain/kotlin/com/example/ffxivprofitestimator/App.kt
@@ -9,9 +9,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
+import com.example.ffxivprofitestimator.Util.LRUCache
 
 class App {
-    val rifleScope = CoroutineScope(SupervisorJob())
+    private val rifleScope = CoroutineScope(SupervisorJob())
     private val platform: Platform = getPlatform()
 
     val testItemName = "rinascita"
@@ -30,7 +31,7 @@ class App {
 
 object XIVAPI : API() {
     override val baseURL = "https://xivapi.com"
-    private val itemCache: MutableMap<Int, CachedItem> = mutableMapOf()
+    private val itemCache: LRUCache<Int, CachedItem> = LRUCache(20)
 
     override suspend fun request(id: Int, contentType: ContentType) {
         val filters: MutableMap<String, Any> = mutableMapOf()
@@ -67,20 +68,88 @@ object XIVAPI : API() {
 }
 
 object UniversalisAPI : API() {
-    override val baseURL: String = "https://universalis.app/api/v2/history"
+    override val baseURL: String = "https://universalis.app/api/v2"
+    private val historyCache: LRUCache<Int, HistoryView> = LRUCache(20)
+    private val dataCenters: MutableList<DataCenter> = mutableListOf()
 
     override suspend fun request(id: Int, contentType: ContentType) {
-        httpClient.get("$baseURL/")
+        when(contentType.getName())
+        {
+            UniversalisContentType.HISTORY.string -> {
+                historyCache[id] = getHistoryView(id)
+            }
+            UniversalisContentType.DC.string -> {
+                dataCenters.clear()
+                dataCenters.addAll(getDatacenters())
+            }
+        }
     }
 
-    suspend fun getPrice()
+    private suspend fun getHistoryView(id: Int): HistoryView
     {
-
+        return httpClient.get("$baseURL/${UniversalisContentType.HISTORY.string}/$id").body()
     }
 
-    fun getDatacenters(): List<String> {
-        TODO()
+    private suspend fun getDatacenters(): List<DataCenter> {
+        return httpClient.get("$baseURL/${UniversalisContentType.DC.string}").body()
     }
+
+    @kotlinx.serialization.Serializable
+    data class DataCenter(
+        val name: String?,
+        val region: String?,
+        val worlds: List<Int>?,
+    )
+
+    @kotlinx.serialization.Serializable
+    data class MinimizedSaleView(
+        // Whether or not the item was high-quality.
+        val hq: Boolean,
+        // The price per unit sold.
+        val pricePerUnit: Int,
+        // The stack size sold.
+        val quantity: Int,
+        // The buyer's character name. This may be null.
+        val buyerName: String?,
+        // Whether or not this was purchased from a mannequin. This may be null.
+        val onMannequin: Boolean?,
+        // The sale time, in seconds since the UNIX epoch.
+        val timestamp: Long,
+        // The world name, if applicable.
+        val worldName: String?,
+        // The world ID, if applicable.
+        val worldID: Int?,
+    )
+
+    @kotlinx.serialization.Serializable
+    data class HistoryView(
+        // The item ID.
+        val itemID: Int,
+        // The world ID, if applicable.
+        val worldID: Int?,
+        // The last upload time for this endpoint, in milliseconds since the UNIX epoch.
+        val lastUploadTime: Long,
+        // The historical sales.
+        val entries: MinimizedSaleView?,
+        // The DC name, if applicable.
+        val dcName: String?,
+        // The region name, if applicable.
+        val regionName: String?,
+        // A map of quantities to sale counts, representing the number of sales of each quantity.
+        val stackSizeHistogram: Map<Int, Int>?,
+        // A map of quantities to NQ sale counts, representing the number of sales of each quantity.
+        val stackSizeHistogramNQ: Map<Int, Int>?,
+        // A map of quantities to HQ sale counts, representing the number of sales of each quantity.
+        val stackSizeHistogramHQ: Map<Int, Int>?,
+        // The average number of sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first).
+        val regularSaleVelocity: Int,
+        // The average number of NQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first).
+        val nqSaleVelocity: Int,
+        // The average number of HQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first).
+        val hqSaleVelocity: Int,
+        // The world name, if applicable.
+        val worldName: String?,
+    )
 }
 
 abstract class API {
@@ -99,7 +168,7 @@ abstract class API {
     )
 
     enum class XIVContentType(val string: String) { ITEM("item"), RECIPE("recipe") }
-    enum class UniversalisContentType(val string: String) { HISTORY("history") }
+    enum class UniversalisContentType(val string: String) { HISTORY("history"), DC("data-centers") }
     sealed class ContentType {
         data class XIV(val type: XIVContentType) : ContentType() {
             override fun getName() = type.string
