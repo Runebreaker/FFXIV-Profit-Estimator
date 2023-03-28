5a76eccaaf331fd264b16056caef2fbb6d91bc41
Runebreaker <gino.schulze-skyfire@web.de>
2023-03-28 18:33:23 +0400

    Moved more stuff to Entity.kt, toString overrides and default values for HistoryView

--- shared/src/commonMain/kotlin/com.jetbrains.handson.kmm.shared.entity/Entity.kt
+++ shared/src/commonMain/kotlin/com.jetbrains.handson.kmm.shared.entity/Entity.kt
@@ -3,18 +3,23 @@ package com.jetbrains.handson.kmm.shared.entity
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+//region Universalis API Data Classes
 @Serializable
 data class World(
     val id: Int,
     val name: String?,
-)
+) {
+    override fun toString(): String = name ?: "Unnamed World"
+}
 
 @Serializable
 data class DataCenter(
     val name: String?,
     val region: String?,
     val worlds: List<Int>?,
-)
+) {
+    override fun toString(): String = name ?: "Unnamed Datacenter"
+}
 
 @Serializable
 data class MinimizedSaleView(
@@ -39,29 +44,56 @@ data class MinimizedSaleView(
 @Serializable
 data class HistoryView(
     // The item ID.
-    val itemID: Int,
+    val itemID: Int = -1,
     // The world ID, if applicable.
-    val worldID: Int?,
+    val worldID: Int? = null,
     // The last upload time for this endpoint, in milliseconds since the UNIX epoch.
-    val lastUploadTime: Long,
+    val lastUploadTime: Long = -1,
     // The historical sales.
-    val entries: MinimizedSaleView?,
+    val entries: MinimizedSaleView? = null,
     // The DC name, if applicable.
-    val dcName: String?,
+    val dcName: String? = null,
     // The region name, if applicable.
-    val regionName: String?,
+    val regionName: String? = null,
     // A map of quantities to sale counts, representing the number of sales of each quantity.
-    val stackSizeHistogram: Map<Int, Int>?,
+    val stackSizeHistogram: Map<Int, Int>? = null,
     // A map of quantities to NQ sale counts, representing the number of sales of each quantity.
-    val stackSizeHistogramNQ: Map<Int, Int>?,
+    val stackSizeHistogramNQ: Map<Int, Int>? = null,
     // A map of quantities to HQ sale counts, representing the number of sales of each quantity.
-    val stackSizeHistogramHQ: Map<Int, Int>?,
+    val stackSizeHistogramHQ: Map<Int, Int>? = null,
     // The average number of sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first).
-    val regularSaleVelocity: Int,
+    val regularSaleVelocity: Int = -1,
     // The average number of NQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first).
-    val nqSaleVelocity: Int,
+    val nqSaleVelocity: Int = -1,
     // The average number of HQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first).
-    val hqSaleVelocity: Int,
+    val hqSaleVelocity: Int = -1,
     // The world name, if applicable.
-    val worldName: String?,
+    val worldName: String? = null,
+) {
+    val valid = itemID >= 0
+}
+//endregion
+
+// region XIV API Data Classes
+@Serializable
+data class Item(
+    val name: String,
+    val id: Int,
+    val icon: String,
+    @SerialName("game_content_links")
+    val links: GameContentLinks,
+) {
+    val recipeIngredients = links.recipe.ingredients
+}
+
+@Serializable
+data class GameContentLinks(
+    val recipe: Recipe,
 )
+
+@Serializable
+data class Recipe(
+    @SerialName("item_result")
+    val ingredients: List<Int>,
+)
+//endregion
