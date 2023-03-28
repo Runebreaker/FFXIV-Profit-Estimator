dd12646fafb79fdab46bd488380009650645f4cc
Runebreaker <gino.schulze-skyfire@web.de>
2023-03-28 18:34:12 +0400

    Some unit tests

--- shared/src/commonTest/kotlin/com/example/ffxivprofitestimator/commonTest.kt
+++ shared/src/commonTest/kotlin/com/example/ffxivprofitestimator/commonTest.kt
@@ -1,18 +1,62 @@
 package com.example.ffxivprofitestimator
 
+import com.example.ffxivprofitestimator.Util.LRUCache
+import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class CommonGreetingTest {
+class APITests {
+    @Test
+    fun testRateLimit() {
+        val testAPI = TestAPI()
+        runBlocking {
+            println("Rate limit: ${testAPI.rateLimit}")
+            for (i in 1..25)
+            {
+                testAPI.request { println("Request $i done at time ${testAPI.referenceTime}") }
+                assertTrue(testAPI.requestsThisSecond <= testAPI.rateLimit)
+            }
+        }
+    }
+}
+
+class CacheTests {
+    private val cacheSize = 5
+    private val testCache: LRUCache<Int, String> = LRUCache(cacheSize)
 
     @Test
-    fun testExample() {
-        assertTrue(App().greet().contains("Hello"), "Check 'Hello' is mentioned")
+    fun testLRUCache() {
+        for (i in 1..12)
+        {
+            testCache[i] = i.toString()
+            assertTrue(testCache.size() <= cacheSize)
+            assertEquals(testCache.first(), i.toString())
+            val list = testCache.getEntries().entries.mapIndexed { index, mutableEntry ->
+                index + 1 to mutableEntry.value
+            }
+            println("Item $i in: ${list.map { "${it.first}=${it.second}" }}")
+            println("Actual size: ${testCache.size()}")
+        }
+
+        println(testCache.getEntries().entries)
     }
 
     @Test
-    fun secondTest() {
-        assertEquals(2,2)
+    fun testLRUFunctions() {
+        for (i in 1..5) {
+            testCache[i] = i.toString()
+        }
+        val elements = testCache.getEntries().entries
+        println("Before: $elements")
+        val lastElement = elements.first()
+        testCache.moveToFront(lastElement.key)
+        assertEquals(testCache.first(), lastElement.value)
+        println("After: ${testCache.getEntries().entries}")
     }
+}
+
+class TestAPI: API() {
+    override val baseURL: String = "test"
+    override val rateLimit: Int = 20
 }
