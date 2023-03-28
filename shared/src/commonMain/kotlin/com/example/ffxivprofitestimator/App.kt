89c926791708916bf2101487329d26963a431627
Runebreaker <gino.schulze-skyfire@web.de>
2023-03-28 18:35:50 +0400

    Major changes, including configurable rate limit

--- shared/src/commonMain/kotlin/com/example/ffxivprofitestimator/App.kt
+++ shared/src/commonMain/kotlin/com/example/ffxivprofitestimator/App.kt
@@ -9,12 +9,19 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
+import kotlinx.datetime.*
 import com.example.ffxivprofitestimator.Util.LRUCache
 import com.jetbrains.handson.kmm.shared.entity.*
+import com.jetbrains.handson.kmm.shared.cache.Database
+import com.jetbrains.handson.kmm.shared.cache.DatabaseDriverFactory
+import io.ktor.http.*
+import kotlinx.coroutines.delay
 
-class App {
+class App(databaseDriverFactory: DatabaseDriverFactory) {
     private val rifleScope = CoroutineScope(SupervisorJob())
     private val platform: Platform = getPlatform()
+
+    private val database = Database(databaseDriverFactory)
 
     val testItemName = "rinascita"
     val rinascitaSwordID = 37742
@@ -22,92 +29,132 @@ class App {
 
     fun greet(): String {
         rifleScope.launch {
-            XIVAPI.getItem(rinascitaSwordID)?.let {
-                it.recipeID
-            } ?: println("Couldn't find item.")
+            XIVAPI.getItem(rinascitaSwordID)?.icon
+                ?: println("Couldn't find item.")
         }
         return "Hello, ${platform.name}!"
     }
+
+    /**
+     * Updates the database by retrieving new information from Universalis.
+     * @return True, if DB was updated. False, if some error occurred retrieving the info.
+     */
+    suspend fun updateDB(force: Boolean = false): Boolean {
+        if (database.getDatacenters().isNotEmpty() && database.getWorlds().isNotEmpty() && !force) return false
+        database.clearDB()
+        val datacenters = UniversalisAPI.getDatacenters() ?: return false
+        val worlds = UniversalisAPI.getWorlds() ?: return false
+        database.fillDB(datacenters, worlds)
+        return true
+    }
+
+    fun getWorldsOfDatacenter(dcName: String): List<World> =
+        database.getWorldsOfDatacenter(dcName)
+
+    fun getDatacenters(): List<DataCenter> =
+        database.getDatacenters()
+
+    fun getWorlds(): List<World> =
+        database.getWorlds()
 }
 
 object XIVAPI : API() {
     override val baseURL = "https://xivapi.com"
-    private val itemCache: LRUCache<Int, CachedItem> = LRUCache(20)
+    override val rateLimit: Int = 20
+    private val itemCache: LRUCache<Int, Item> = LRUCache(20)
+    private val iconCache: LRUCache<Int, ByteArray> = LRUCache(20)
 
-    override suspend fun request(id: Int, contentType: ContentType) {
-        val filters: MutableMap<String, Any> = mutableMapOf()
-        filters["columns"] = "ID,Name,GameContentLinks.Recipe.ItemResult"
-        filters["snake_case"] = 1
+    private val filters: Map<String, Any> = mapOf(
+        "columns" to "ID,Icon,Name,GameContentLinks.Recipe.ItemResult",
+        "snake_case" to 1
+    )
 
-        var requestURL = "$baseURL/${contentType.getName()}/$id"
-        if (filters.isNotEmpty()) requestURL += "?" + filters.map { "${it.key}=${it.value}" }
-            .joinToString("&")
+    /**
+     * Get the item with the specified id from the cache, or from XIVAPI, if not present.
+     * @return The Item, if it exists or null otherwise.
+     * @param id The specified ID of the Item you want to retrieve.
+     */
+    suspend fun getItem(id: Int): Item? {
+        if (!itemCache.containsKey(id)) {
+            var requestURL = "$baseURL/item/$id"
+            if (filters.isNotEmpty()) requestURL += "?" + filters.map { "${it.key}=${it.value}" }
+                .joinToString("&")
 
-        val requestedItem: Item = httpClient.get(requestURL).body()
-        itemCache[id] = CachedItem(
-            requestedItem.name,
-            requestedItem.game_content_links.recipe.item_result[0]
-        )
-    }
-
-    suspend fun getItem(id: Int): CachedItem?
-    {
-        if (!itemCache.contains(id)) request(id, ContentType.XIV(XIVContentType.ITEM))
+            itemCache[id] = request {
+                    httpClient ->
+                httpClient.get(requestURL).body()
+            } ?: return null
+            iconCache[id] = request { httpClient ->
+                httpClient.get("$baseURL${itemCache[id]?.icon ?: return@request null}").body()
+            } ?: return null
+        }
         return itemCache[id]
     }
 
-    data class CachedItem(val name: String, val recipeID: Int)
-
-    @kotlinx.serialization.Serializable
-    data class Item(val name: String, val id: Int, val game_content_links: GameContentLinks)
-
-    @kotlinx.serialization.Serializable
-    data class GameContentLinks(val recipe: Recipe)
-
-    @kotlinx.serialization.Serializable
-    data class Recipe(val item_result: List<Int>)
+    /**
+     * Retrieves an image at a given URL as a ByteArray.
+     * @return The byte array, if successful.
+     * @param url The url of the image.
+     */
+    suspend fun getIconAsByteArray(url: Url): ByteArray? =
+        request { httpClient ->
+            httpClient.get("").body()
+        }
 }
 
 object UniversalisAPI : API() {
     override val baseURL: String = "https://universalis.app/api/v2"
+    override val rateLimit: Int = 25
     private val historyCache: LRUCache<Int, HistoryView> = LRUCache(20)
-    private val dataCenters: MutableList<DataCenter> = mutableListOf()
-    private val worlds: MutableList<World> = mutableListOf()
 
-    override suspend fun request(id: Int, contentType: ContentType) {
-        when(contentType.getName())
-        {
-            UniversalisContentType.HISTORY.string -> {
-                historyCache[id] = getHistoryView(id)
-            }
-            UniversalisContentType.DC.string -> {
-                dataCenters.clear()
-                dataCenters.addAll(getDatacenters())
-            }
-            UniversalisContentType.WORLD.string -> {
-                worlds.clear()
-                worlds.addAll(getWorlds())
-            }
+    suspend fun getItem(id: Int): HistoryView? {
+        if (!historyCache.moveToFront(id)) return null
+        getHistoryView(id)?.let { historyView ->
+            historyCache[id] = historyView
+        } ?: return null
+        return historyCache.first()
+    }
+
+    fun getCachedItems(): LinkedHashMap<Int, HistoryView> =
+        historyCache.getEntries()
+
+    private suspend fun getHistoryView(id: Int): HistoryView? =
+        request { httpClient ->
+            httpClient.get("$baseURL/history/$id").body()
         }
-    }
 
-    private suspend fun getHistoryView(id: Int): HistoryView
-    {
-        return httpClient.get("$baseURL/${UniversalisContentType.HISTORY.string}/$id").body()
-    }
+    suspend fun getDatacenters(): List<DataCenter>? =
+        request { httpClient ->
+            httpClient.get("$baseURL/data-centers").body()
+        }
 
-    suspend fun getDatacenters(): List<DataCenter> {
-        return httpClient.get("$baseURL/${UniversalisContentType.DC.string}").body()
-    }
-
-    suspend fun getWorlds(): List<World> {
-        return httpClient.get("$baseURL/${UniversalisContentType.WORLD.string}").body()
-    }
+    suspend fun getWorlds(): List<World>? =
+        request { httpClient ->
+            httpClient.get("$baseURL/worlds").body()
+        }
 }
 
+/**
+ * A small baseline for API implementations. Includes rate limiting. To make requests,
+ * just use request { httpClient -> ...code... } to get access to the http client
+ * and to have automatic enforcement of the rate limit.
+ */
 abstract class API {
+    /**
+     * Base URL of the used API
+     */
     abstract val baseURL: String
-    val httpClient = HttpClient {
+
+    /**
+     * API Rate limit in requests per second
+     */
+    abstract val rateLimit: Int
+
+    var requestsThisSecond: Int = 0
+        private set
+    var referenceTime: Long = 0
+
+    private val httpClient = HttpClient {
         install(ContentNegotiation) {
             json(Json {
                 ignoreUnknownKeys = true
@@ -116,21 +163,22 @@ abstract class API {
         }
     }
 
-    abstract suspend fun request(
-        id: Int, contentType: ContentType
-    )
-
-    enum class XIVContentType(val string: String) { ITEM("item"), RECIPE("recipe") }
-    enum class UniversalisContentType(val string: String) { HISTORY("history"), DC("data-centers"), WORLD("worlds") }
-    sealed class ContentType {
-        data class XIV(val type: XIVContentType) : ContentType() {
-            override fun getName() = type.string
+    suspend fun <T> request(requestLambda: suspend (httpClient: HttpClient) -> T?): T? {
+        //region rate limitation
+        var currentTime = Clock.System.now().toEpochMilliseconds()
+        var millisUntilReset = referenceTime + 1000 - currentTime
+        // Delay until new rate limit window with a small buffer
+        if (requestsThisSecond >= rateLimit) {
+            delay(millisUntilReset + 100)
+            currentTime = Clock.System.now().toEpochMilliseconds()
+            millisUntilReset = referenceTime + 1000 - currentTime
         }
-
-        data class Universalis(val type: UniversalisContentType) : ContentType() {
-            override fun getName() = type.string
+        if (millisUntilReset <= 0) {
+            requestsThisSecond = 0
+            referenceTime = currentTime
         }
-
-        abstract fun getName(): String
+        requestsThisSecond++
+        //endregion
+        return requestLambda(httpClient)
     }
 }
