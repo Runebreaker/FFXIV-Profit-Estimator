dbbf146878d55b37dd00b9305f50c73bd13d099b
Runebreaker <gino.schulze-skyfire@web.de>
2023-03-28 18:31:03 +0400

    New stuff for the LRUCache

--- shared/src/commonMain/kotlin/com/example/ffxivprofitestimator/Util.kt
+++ shared/src/commonMain/kotlin/com/example/ffxivprofitestimator/Util.kt
@@ -8,14 +8,38 @@ object Util {
 
         operator fun set(key: K, value: V)
         {
+            map.remove(key)
             map[key] = value
-            if (map.entries.size > size) map.remove(map.entries.last().key)
+            if (map.entries.size > size) map.remove(map.entries.first().key)
         }
+
+        fun moveToFront(key: K): Boolean {
+            val tempVal = map[key] ?: return false
+            map.remove(key)
+            map[key] = tempVal
+            return true
+        }
+
+        fun first(): V? {
+            if (map.entries.isEmpty()) return null
+            return map.entries.last().value
+        }
+
+        fun last(): V? {
+            if (map.entries.isEmpty()) return null
+            return map.entries.first().value
+        }
+
+        fun getEntries(): LinkedHashMap<K, V> = map
+
+        fun getOrDefault(key: K, default: V): V = map[key] ?: default
 
         fun size() = map.size
 
         fun clear() = map.clear()
 
-        fun contains(key: K) = map.containsKey(key)
+        fun containsKey(key: K) = map.containsKey(key)
+
+        fun containsValue(value: V) = map.containsValue(value)
     }
 }
