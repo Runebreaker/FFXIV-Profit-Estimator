13fec7d7f02391c59e4bad7ecfb0d210f294ff6b
Runebreaker <gino.schulze-skyfire@web.de>
2023-03-29 23:14:12 +0400

    Some UI changes, first experiments using scaffolds and drawers

--- FFXIV_Profit_Estimator/src/main/java/com/example/ffxivprofitestimator/android/MainActivity.kt
+++ FFXIV_Profit_Estimator/src/main/java/com/example/ffxivprofitestimator/android/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
@@ -17,10 +18,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.ffxivprofitestimator.App
 import com.jetbrains.handson.kmm.shared.cache.DatabaseDriverFactory
 import com.jetbrains.handson.kmm.shared.entity.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 lateinit var app: App
 
@@ -47,61 +49,97 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MenuView(servers: List<DataCenter>)
-{
+fun MenuView(servers: List<DataCenter>) {
+    var scaffoldState: ScaffoldState = rememberScaffoldState()
+    var coroutineScope: CoroutineScope = rememberCoroutineScope()
     var chosenDC: DataCenter? by remember { mutableStateOf(null) }
     var chosenWorld: World? by remember { mutableStateOf(null) }
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.TopStart)
-    ) {
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(0.05f)
-            .background(Color.White)
-        ) {
-            Text(text = "FFXIV Profit Estimator",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Serif,
-                modifier = Modifier
-                    .align(Alignment.Center)
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                backgroundColor = Color.Black,
+                contentColor = Color.White,
+                title = {
+                    Text(text = "FFXIV Profit Estimator")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        coroutineScope.launch {
+                            scaffoldState.drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    }) {
+                        Icon(
+                            Icons.Default.Menu,
+                            contentDescription = "Menu"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            Icons.Default.Search,
+                            contentDescription = "Search"
+                        )
+                    }
+                }
             )
-        }
-        Column(verticalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxSize()
-                .wrapContentSize(Alignment.TopStart)
-                .padding(Dp(10f))
-        )
-        {
-            Row(horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier
-                .fillMaxWidth()
+        },
+        bottomBar = {
+            BottomAppBar(
+                backgroundColor = Color.Black,
+                contentColor = Color.White
             ) {
                 SelectionButton(
-                    modifier = Modifier.weight(1f).padding(5.dp),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(5.dp),
                     activeElement = chosenDC,
                     elements = servers,
-                    onActiveElementChange = { chosenDC = it },
+                    onActiveElementChange = {
+                        chosenDC = it
+                        chosenWorld = null
+                    },
                 )
                 SelectionButton(
-                    modifier = Modifier.weight(1f).padding(5.dp),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(5.dp),
                     activeElement = chosenWorld,
                     elements = chosenDC?.name?.let { app.getWorldsOfDatacenter(it) } ?: emptyList(),
                     onActiveElementChange = { chosenWorld = it },
                     enabled = chosenDC != null
                 )
             }
+        },
+        drawerContent = {
+            Text("Entry 1")
+            Text("Entry 2")
+            Divider()
+            Text("Entry 3")
+        }
+    ) {
+        Box(
+            modifier = Modifier.padding(it)
+        ) {
+            Text("Ayyy lmao")
         }
     }
 }
 
 @Composable
-fun <T> SelectionButton(modifier: Modifier, enabled: Boolean = true, activeElement: T, elements: List<T>, onActiveElementChange: (T) -> Unit)
-{
+fun <T> SelectionButton(
+    modifier: Modifier,
+    enabled: Boolean = true,
+    activeElement: T,
+    elements: List<T>,
+    onActiveElementChange: (T) -> Unit
+) {
     var expanded by remember { mutableStateOf(false) }
-    Button(onClick = { expanded = true },
+    Button(
+        onClick = { expanded = true },
         shape = CutCornerShape(30),
         border = BorderStroke(Dp(2f), Color.Black),
         colors = ButtonDefaults.buttonColors(
@@ -113,7 +151,8 @@ fun <T> SelectionButton(modifier: Modifier, enabled: Boolean = true, activeEleme
         modifier = modifier,
         enabled = enabled
     ) {
-        Text(text = activeElement.toString(),
+        Text(
+            text = activeElement?.toString() ?: "Unnamed",
             fontFamily = FontFamily.SansSerif,
             fontWeight = FontWeight.Bold
         )
@@ -125,7 +164,8 @@ fun <T> SelectionButton(modifier: Modifier, enabled: Boolean = true, activeEleme
                     onActiveElementChange(it)
                     expanded = false
                 }) {
-                    Text(it.toString(),
+                    Text(
+                        it?.toString() ?: "Unnamed",
                         fontFamily = FontFamily.Monospace,
                         fontWeight = FontWeight.Bold
                     )
